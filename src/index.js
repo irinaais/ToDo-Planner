8dab2f4fc0b7acbc7fcf28c2375a1bd553a4f45c
Iris <irinaais90@gmail.com>
2023-04-04 12:50:10 +0400

    feat: add button plus

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// function showTasks() {
+//   let storageSize = localStorage.length;
+//   if (storageSize > 0) {
+//     for (let i = 0; i < storageSize; i++) {
+//       let key = localStorage.key(i);
+//       if ()
+//     }
+//   }
+// }
