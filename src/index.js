2946699f41eb746e50aea363cc868dadd2b1d0ca
Iris <irinaais90@gmail.com>
2023-04-07 12:58:33 +0400

    feat: when we click on the add-button, the task is added and the input is cleared

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-// function showTasks() {
-//   let storageSize = localStorage.length;
-//   if (storageSize > 0) {
-//     for (let i = 0; i < storageSize; i++) {
-//       let key = localStorage.key(i);
-//       if ()
-//     }
-//   }
-// }
+const addButton = document.querySelector('.button_variant_add');
+const newTaskInput = document.querySelector('.form__text');
+
+const allTasks = [];
+
+function deleteInputValue() {
+  newTaskInput.value = '';
+}
+
+function addNewTask(inputValue) {
+  const id = allTasks.length + 1;
+  const task = {
+    id,
+    inputValue,
+    isDone: false
+  }
+  allTasks.push(task);
+  deleteInputValue();
+}
+
+addButton.addEventListener('click', () => {
+  if (newTaskInput.value) {
+    addNewTask(newTaskInput.value);
+  }
+});
