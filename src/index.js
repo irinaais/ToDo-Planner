2e0d601d9ccd54c5be4172abc13b88e8a5eb2370
Iris <irinaais90@gmail.com>
2023-04-10 12:43:10 +0400

    feat: add checking new tasks for repetition

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
+// function showTasks() {
+//   let storageSize = localStorage.length;
+//   if (storageSize > 0) {
+//     for (let i = 0; i < storageSize; i++) {
+//       let key = localStorage.key(i);
+//       if ()
+//     }
+//   }
+// }
+
 const addButton = document.querySelector('.button_variant_add');
 const newTaskInput = document.querySelector('.form__text');
 
 const allTasks = [];
+
+function checkForDuplicate(inputValue) {
+  let isDuplicate = false;
+  allTasks.forEach((task) => {
+    if (task.inputValue === inputValue) {
+      alert('Такая задача уже существует');
+      isDuplicate = true;
+    }
+  });
+  return isDuplicate;
+}
 
 function deleteInputValue() {
   newTaskInput.value = '';
 }
 
 function addNewTask(inputValue) {
+  if (checkForDuplicate(inputValue)) {
+    return;
+  }
+
   const id = allTasks.length + 1;
   const task = {
     id,
