0ac0d6156b1d7bfc7e258af5a21930bc65680246
Iris <irinaais90@gmail.com>
2023-04-25 18:18:03 +0400

    refactor: adding tasks using template

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const addButton = document.querySelector('.button_variant_add');
 const newTaskInput = document.querySelector('.form__text');
 const listOfTasks = document.querySelector('.list');
 const form = document.querySelector('.form');
+const template = document.querySelector('#template');
 
 let allTasks = [];
 
@@ -37,54 +38,42 @@ function addNewTask(inputValue) {
 }
 
 function renderAllTask(tasks) {
-  let htmlList = '';
-
-  tasks.forEach(task => {
-    const taskClass = task.isDone
-      ? 'list__task list__task_completed'
-      : 'list__task';
-
-    const isChecked = task.isDone ? 'checked' : '';
-
-    const htmlTask = `
-      <div class="${taskClass}" id = "${task.id}">
-        <label class="list__label">
-            <input class="list__checkbox-input" type="checkbox" ${isChecked}>
-            <span class="list__checkbox-toggle"></span>
-        </label>
-        <div class="list__task-text">${task.inputValue}</div>
-        <button class="button button_variant_edit" type="button" aria-label="Редактировать задачу"></button>
-        <button class="button button_variant_delete" type="button" aria-label="Удалить задачу"></button>
-      </div>
-    `
-
-    htmlList = htmlList + htmlTask;
-  })
-
-  listOfTasks.innerHTML = htmlList;
+  listOfTasks.innerHTML = '';
+  for (const task of tasks) {
+    const templateItem = template.content.cloneNode(true);
+    const li = templateItem.querySelector('li');
+    // const input = templateItem.querySelector('.list__checkbox-input');
+    const p = templateItem.querySelector('.list__task-text');
+    // if (task.isDone) li.classList.add('list__task_completed') && input.setAttribute('checked', 'true');
+    // input.checked = task.isDone;
+    li.id = task.id;
+    p.textContent = task.inputValue;
+    listOfTasks.append(templateItem);
+  }
 }
 
-function changeTaskStatus(id, status, tasks) {
-  const task = tasks.find(function (task) {
-    return task.id === id;
-  })
-  task.isDone = !task.isDone;
-  localStorage.setItem('allTasks', JSON.stringify(allTasks));
-}
+// function changeTaskStatus(id, status, tasks) {
+//   const task = tasks.find(function (task) {
+//     return task.id === id;
+//   })
+//   task.isDone = !task.isDone;
+//   localStorage.setItem('allTasks', JSON.stringify(allTasks));
+// }
 
-function deleteTask(id, tasks) {
-  allTasks = tasks.filter(function (task) {
-    return task.id !== id;
-  });
-  localStorage.setItem('allTasks', JSON.stringify(allTasks));
-}
+// function deleteTask(id, tasks) {
+//   allTasks = tasks.filter(function (task) {
+//     return task.id !== id;
+//   });
+//   localStorage.setItem('allTasks', JSON.stringify(allTasks));
+// }
 
 function addNewTaskAndRenderAllTasks() {
   if (newTaskInput.value && !checkForDuplicate(newTaskInput.value)) {
     addNewTask(newTaskInput.value);
+    localStorage.setItem('allTasks', JSON.stringify(allTasks));
     deleteInputValue();
     renderAllTask(allTasks);
-    localStorage.setItem('allTasks', JSON.stringify(allTasks));
+    console.log(allTasks);
   }
 }
 
@@ -97,23 +86,23 @@ form.addEventListener('submit', (evt) => {
   addNewTaskAndRenderAllTasks();
 });
 
-listOfTasks.addEventListener('click', (evt) => {
-  const target = evt.target;
-
-  const checkbox = target.classList.contains('list__checkbox-toggle');
-  const deleteButton = target.classList.contains('button_variant_delete');
-
-  if (checkbox) {
-    const isInputChecked = !evt.target.previousElementSibling.checked;
-    const task = evt.target.parentElement.parentElement;
-
-    changeTaskStatus(task.id, isInputChecked, allTasks)
-    renderAllTask(allTasks);
-
-  } else if (deleteButton) {
-    const task = evt.target.parentElement;
-
-    deleteTask(task.id, allTasks);
-    renderAllTask(allTasks);
-  }
-});
+// listOfTasks.addEventListener('click', (evt) => {
+//   const target = evt.target;
+//
+//   const checkbox = target.classList.contains('list__checkbox-toggle');
+//   const deleteButton = target.classList.contains('button_variant_delete');
+//
+//   if (checkbox) {
+//     const isInputChecked = !evt.target.previousElementSibling.checked;
+//     const task = evt.target.parentElement.parentElement;
+//
+//     changeTaskStatus(task.id, isInputChecked, allTasks)
+//     renderAllTask(allTasks);
+//
+//   } else if (deleteButton) {
+//     const task = evt.target.parentElement;
+//
+//     deleteTask(task.id, allTasks);
+//     renderAllTask(allTasks);
+//   }
+// });
