3d9ad7e9b640ce28c90f4a54875f6da77964b28c
Iris <irinaais90@gmail.com>
2023-04-27 16:06:49 +0400

    feat: added changing task's text on the editButton and confirmButton click

--- src/index.js
+++ src/index.js
@@ -58,6 +58,15 @@ function changeTaskStatus(id, status, tasks) {
   localStorage.setItem('allTasks', JSON.stringify(allTasks));
 }
 
+function changeTaskText(id, text, tasks) {
+  const task = tasks.find(function (task) { //TODO вынести в отдельную ф-ю и в changeTaskStatus тоже
+    return task.id === id;
+  })
+  task.inputValue = text;
+  task.id = text;
+  localStorage.setItem('allTasks', JSON.stringify(allTasks));
+}
+
 function deleteTask(id, tasks) {
   allTasks = tasks.filter(function (task) {
     return task.id !== id;
@@ -89,6 +98,7 @@ listOfTasks.addEventListener('click', (evt) => {
   const checkbox = target.classList.contains('list__checkbox-toggle');
   const deleteButton = target.classList.contains('button_variant_delete');
   const editButton = target.classList.contains('button_variant_edit');
+  const confirmButton = target.classList.contains('button_variant_confirm');
 
   if (checkbox) {
     const isInputChecked = !target.previousElementSibling.checked;
@@ -101,16 +111,23 @@ listOfTasks.addEventListener('click', (evt) => {
     deleteTask(task.id, allTasks);
     renderAllTask(allTasks);
 
-  } else if (editButton) { //TODO вынести в отдельную функцию
+  } else if (editButton) {
     const li = target.parentElement;
     const p = target.previousElementSibling;
     const input = document.createElement('input');
     const deleteButton = target.nextElementSibling;
     input.classList.add('list__task-input');
+    input.value = p.innerText;
     li.replaceChild(input, p);
     target.classList.remove('button_variant_edit');
     target.classList.add('button_variant_confirm');
     target.classList.add('button_visible');
     deleteButton.classList.add('button_visible');
+
+  } else if (confirmButton) {
+    const input = target.previousElementSibling;
+    const task = target.parentElement;
+    changeTaskText(task.id, input.value, allTasks);
+    renderAllTask(allTasks); //TODO убрать ф-ю? итак все работает
   }
 });
