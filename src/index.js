ec3844d62f4e312a45c550b371a83fe0565273fd
Iris <irinaais90@gmail.com>
2023-05-16 00:40:43 +0300

    refactor: moved the code to the initializing function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,86 @@
-const addButton = document.querySelector('.button_variant_add');
-const newTaskInput = document.querySelector('.form__text');
-const listOfTasks = document.querySelector('.list');
-const form = document.querySelector('.form');
-const template = document.querySelector('#template');
-
-let allTasks = [];
+let addButton;
+let newTaskInput;
+let listOfTasks;
+let form;
+let template;
+let allTasks;
 
 (async () => {
+  addButton = document.querySelector('.button_variant_add');
+  newTaskInput = document.querySelector('.form__text');
+  listOfTasks = document.querySelector('.list');
+  form = document.querySelector('.form');
+  template = document.querySelector('#template');
+
+  allTasks = [];
+
   allTasks = await readTasks();
   if (allTasks.length > 0) {
     renderAllTask(allTasks);
   }
+
+  addButton.addEventListener('click', () => {
+    addNewTaskAndRenderAllTasks();
+  });
+
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    addNewTaskAndRenderAllTasks();
+  });
+
+  listOfTasks.addEventListener('click', (evt) => {
+    const target = evt.target;
+
+    const checkbox = target.classList.contains('list__checkbox-toggle');
+    const deleteButton = target.classList.contains('button_variant_delete');
+    const editButton = target.classList.contains('button_variant_edit');
+    const confirmButton = target.classList.contains('button_variant_confirm');
+    const cancelButton = target.classList.contains('button_variant_cancel');
+
+    if (checkbox) {
+      const isInputChecked = !target.previousElementSibling.checked;
+      const task = target.parentElement.parentElement;
+      changeTaskStatus(task.id, isInputChecked, allTasks)
+      renderAllTask(allTasks);
+
+    } else if (deleteButton) {
+      const task = target.parentElement;
+      deleteTask(task.id, allTasks);
+      renderAllTask(allTasks);
+
+    } else if (editButton) {
+      const li = target.parentElement;
+      const p = target.previousElementSibling;
+      const editButton = target;
+      const deleteButton = target.nextElementSibling;
+      changeTask(p, li, editButton, deleteButton);
+      addEnterEventListener();
+
+    } else if (confirmButton) {
+      const input = target.previousElementSibling;
+      const task = target.parentElement;
+      changeTaskAndRenderAllTasks(input, task);
+
+    } else if (cancelButton) {
+      renderAllTask(allTasks);
+    }
+  });
+
+  listOfTasks.addEventListener('dblclick', (evt) => {
+    const taskText = evt.target.classList.contains('list__task-text');
+
+    if (taskText) {
+      const p = evt.target;
+      const li = evt.target.parentElement;
+      const editButton = evt.target.nextElementSibling;
+      const deleteButton = evt.target.nextElementSibling.nextElementSibling;
+      changeTask(p, li, editButton, deleteButton);
+      addEnterEventListener();
+    }
+  })
 })();
 
+//=================================================================
 async function readTasks() {
   try {
     let allTasks = localStorage.getItem('allTasks');
@@ -147,63 +215,3 @@ function changeTaskAndRenderAllTasks(input, task) {
     alert('Введите текст задачи');
   }
 }
-
-addButton.addEventListener('click', () => {
-  addNewTaskAndRenderAllTasks();
-});
-
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  addNewTaskAndRenderAllTasks();
-});
-
-listOfTasks.addEventListener('click', (evt) => {
-  const target = evt.target;
-
-  const checkbox = target.classList.contains('list__checkbox-toggle');
-  const deleteButton = target.classList.contains('button_variant_delete');
-  const editButton = target.classList.contains('button_variant_edit');
-  const confirmButton = target.classList.contains('button_variant_confirm');
-  const cancelButton = target.classList.contains('button_variant_cancel');
-
-  if (checkbox) {
-    const isInputChecked = !target.previousElementSibling.checked;
-    const task = target.parentElement.parentElement;
-    changeTaskStatus(task.id, isInputChecked, allTasks)
-    renderAllTask(allTasks);
-
-  } else if (deleteButton) {
-    const task = target.parentElement;
-    deleteTask(task.id, allTasks);
-    renderAllTask(allTasks);
-
-  } else if (editButton) {
-    const li = target.parentElement;
-    const p = target.previousElementSibling;
-    const editButton = target;
-    const deleteButton = target.nextElementSibling;
-    changeTask(p, li, editButton, deleteButton);
-    addEnterEventListener();
-
-  } else if (confirmButton) {
-    const input = target.previousElementSibling;
-    const task = target.parentElement;
-    changeTaskAndRenderAllTasks(input, task);
-
-  } else if (cancelButton) {
-    renderAllTask(allTasks);
-  }
-});
-
-listOfTasks.addEventListener('dblclick', (evt) => {
-  const taskText = evt.target.classList.contains('list__task-text');
-
-  if (taskText) {
-    const p = evt.target;
-    const li = evt.target.parentElement;
-    const editButton = evt.target.nextElementSibling;
-    const deleteButton = evt.target.nextElementSibling.nextElementSibling;
-    changeTask(p, li, editButton, deleteButton);
-    addEnterEventListener();
-  }
-})
